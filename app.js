925deaf90665c394fc1fa0020375f945c29e193a
ninspyth <94296072+ninspyth@users.noreply.github.com>
2023-02-27 21:25:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+﻿const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('input[type=text]');
+
+let searchUrl = '';
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    searchUrl = searchInput.value;
+    if (isPhishingWebsite(searchUrl)) {
+        document.getElementById('result').innerHTML = 'The Given URL is of a Phishing website';
+    }
+    else {
+        document.getElementById('result').innerHTML = 'The Given URL is a legit website';
+    }
+});
+
+function isPhishingWebsite(url) {
+    const features = {
+        isatpresent: function () {
+            for (let i = 0; i < url.length; i++) {
+                if (url[i] === '@') {
+                    console.log(url.length);
+                    return true;
+                }
+                else { 
+                    return false;
+                }
+    
+            }
+
+        }
+    };
+    for (x in features) {
+        if (x) {
+            return true;
+        }
+        else {
+            return false; 
+        }
+    }
+    
+}
